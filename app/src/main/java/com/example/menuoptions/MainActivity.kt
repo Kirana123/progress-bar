7692ac6e046b22c21b02ffefb6c56e736e34511c
kirana123 <email@example.comtsanusha26@gmail.com>
2022-02-08 10:32:19 +0530

    Progress bar - Loader & Seekbar - Toaster

--- app/src/main/java/com/example/menuoptions/MainActivity.kt
+++ app/src/main/java/com/example/menuoptions/MainActivity.kt
@@ -2,11 +2,9 @@ package com.example.menuoptions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
 import android.os.Handler
 import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,11 +17,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val seek = findViewById<SeekBar>(R.id.seekBar)
+
         progressBar = findViewById<ProgressBar>(R.id.progress_Bar) as ProgressBar
 
         txtView = findViewById<TextView>(R.id.text_view)
 
         val btn = findViewById<Button>(R.id.show_button)
+
+        seek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seek: SeekBar,progress: Int, fromUser: Boolean) {
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                Toast.makeText(this@MainActivity,
+                    "Progress is: " + seek.progress + "%",
+                    Toast.LENGTH_SHORT).show()
+            }
+        })
 
         btn.setOnClickListener {
             progressBar!!.visibility = View.VISIBLE
